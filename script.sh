e3ce772b6e675fcfc9fdfeaad43baab5d28aee68
Eugene <ebagdasaryan@me.com>
2023-02-07 11:32:28 -0500

    updates

--- script.sh
+++ script.sh
@@ -6,6 +6,6 @@ set PYTHONPATH="."
 for (( k = 1; k < 20; ++k ));
   do
     iteration=$((5*$k))
-    python examples/mnist/mnist_example.py --epochs 1 --poison_ratio $iteration
+    python mnist_example.py --epochs 1 --poison_ratio $iteration
 
   done
